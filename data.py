ace86e436889b3ef31268589bf8972c03f1bf925
vansky <vansky@users.noreply.github.com>
2018-01-26 12:03:53 -0500

    made unking backwards-compatible
    added function to convert text vocab file (1 word per line) into a usable vocab for the lm

--- data.py
+++ data.py
@@ -5,12 +5,26 @@ import gzip
 
 from nltk import sent_tokenize
 
+class convertvocab(object):
+    def __init__(self, load_from, save_to):
+        self.dictionary = Dictionary()
+        self.loadme = self.load_dict(load_from)
+        self.save_to = self.save_dict(save_to)
+
+    def save_dict(self, path):
+        with open(path, 'wb') as f:
+            torch.save(self.dictionary, f, pickle_module=dill)
+
+    def load_dict(self, path):
+        assert os.path.exists(path)
+        with open(path, 'r') as f:
+            for line in f:
+                self.dictionary.add_word(line.strip())
+
 class Dictionary(object):
     def __init__(self):
         self.word2idx = {}
         self.idx2word = []
-        self.word2idx["<unk>"] = 0
-        self.idx2word.append("<unk>")
 
     def add_word(self, word):
         if word not in self.word2idx:
@@ -145,7 +159,7 @@ class SentenceCorpus(object):
                             words = line.split() + ['<eos>']
                         for word in words:
                             if word not in self.dictionary.word2idx:
-                                ids[token] = self.dictionary.word2idx["<unk>"]
+                                ids[token] = self.dictionary.add_word("<unk>")
                             else:
                                 ids[token] = self.dictionary.word2idx[word]
                             token += 1
@@ -177,7 +191,7 @@ class SentenceCorpus(object):
                             words = line.split() + ['<eos>']
                         for word in words:
                             if word not in self.dictionary.word2idx:
-                                ids[token] = self.dictionary.word2idx["<unk>"]
+                                ids[token] = self.dictionary.add_word("<unk>")
                             else:
                                 ids[token] = self.dictionary.word2idx[word]
                             token += 1
@@ -201,7 +215,7 @@ class SentenceCorpus(object):
                         token = 0
                         for word in words:
                             if word not in self.dictionary.word2idx:
-                                ids[token] = self.dictionary.word2idx["<unk>"]
+                                ids[token] = self.dictionary.add_word("<unk>")
                             else:
                                 ids[token] = self.dictionary.word2idx[word]
                             token += 1
@@ -219,7 +233,7 @@ class SentenceCorpus(object):
                         token = 0
                         for word in words:
                             if word not in self.dictionary.word2idx:
-                                ids[token] = self.dictionary.word2idx["<unk>"]
+                                ids[token] = self.dictionary.add_word("<unk>")
                             else:
                                 ids[token] = self.dictionary.word2idx[word]
                             token += 1
