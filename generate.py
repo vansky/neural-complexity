dc867a77d956a12d92dfd9b37ca21cb2c8f2f0a8
vansky <vansky@users.noreply.github.com>
2018-06-01 17:38:22 -0400

    unified cli args

--- generate.py
+++ generate.py
@@ -15,13 +15,13 @@ import data
 parser = argparse.ArgumentParser(description='PyTorch PTB Language Model')
 
 # Model parameters.
-parser.add_argument('--data', type=str, default='./data/penn',
+parser.add_argument('--data_dir', type=str, default='./data/wikitext-2',
                     help='location of the data corpus')
-parser.add_argument('--checkpoint', type=str, default='./model.pt',
+parser.add_argument('--model_file', type=str, default='./model.pt',
                     help='model checkpoint to use')
 parser.add_argument('--outf', type=str, default='generated.txt',
                     help='output file for generated text')
-parser.add_argument('--words', type=int, default='1000',
+parser.add_argument('--numwords', type=int, default='1000',
                     help='number of words to generate')
 parser.add_argument('--seed', type=int, default=1111,
                     help='random seed')
@@ -29,9 +29,9 @@ parser.add_argument('--cuda', action='store_true',
                     help='use CUDA')
 parser.add_argument('--temperature', type=float, default=1.0,
                     help='temperature - higher will increase diversity')
-parser.add_argument('--log-interval', type=int, default=100,
+parser.add_argument('--log_interval', type=int, default=100,
                     help='reporting interval')
-parser.add_argument('--lm_data', type=str, default='lm_data.bin',
+parser.add_argument('--vocab_file', type=str, default='vocab.txt',
                     help='path to save the LM data')
 parser.add_argument('--testfname', type=str, default='test.txt',
                     help='name of the test file')
@@ -48,7 +48,7 @@ if torch.cuda.is_available():
 if args.temperature < 1e-3:
     parser.error("--temperature has to be greater or equal 1e-3")
 
-with open(args.checkpoint, 'rb') as f:
+with open(args.model_file, 'rb') as f:
     model = torch.load(f)
 model.eval()
 
@@ -57,7 +57,7 @@ if args.cuda:
 else:
     model.cpu()
 
-corpus = data.SentenceCorpus(args.data, args.lm_data, True,
+corpus = data.SentenceCorpus(args.data_dir, args.vocab_file, True,
                              testfname=args.testfname)
 
 ntokens = len(corpus.dictionary)
@@ -67,7 +67,7 @@ if args.cuda:
     input.data = input.data.cuda()
 
 with open(args.outf, 'w') as outf:
-    for i in range(args.words):
+    for i in range(args.numwords):
         output, hidden = model(input, hidden)
         word_weights = output.squeeze().data.div(args.temperature).exp().cpu()
         word_idx = torch.multinomial(word_weights, 1)[0]
@@ -77,4 +77,4 @@ with open(args.outf, 'w') as outf:
         outf.write(word + ('\n' if i % 20 == 19 else ' '))
 
         if i % args.log_interval == 0:
-            print('| Generated {}/{} words'.format(i, args.words))
+            print('| Generated {}/{} words'.format(i, args.numwords))
